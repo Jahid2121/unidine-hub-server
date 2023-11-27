c171f4d9efdd79c4f07259803ab82031b53a716d
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-27 10:42:04 +0600

    implement get method for fetching meal details by id

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pu45iww.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,6 +38,13 @@ async function run() {
     app.get('/meal', async (req, res) => {
     const result = await mealCollection.find().toArray();
     res.send(result)
+    })
+
+    app.get('/meal/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await mealCollection.findOne(query)
+      res.send(result)
     })
 
 
