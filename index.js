7b1b9a7b52013bb5a6964d26d98851535fe4bd8a
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-28 11:29:34 +0600

    Implement users retrival api endpoint

--- index.js
+++ index.js
@@ -111,6 +111,11 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users', async ( req, res ) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
