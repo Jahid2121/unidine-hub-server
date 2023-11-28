73745bb38d3b04535bc65ed4750e8dca4e2dcc02
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-28 12:27:37 +0600

    Add Jwt token for user verify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+// const verifyAdmin = async( req, res, next) => {
+
+// }
 
 
 
@@ -99,6 +104,17 @@ async function run() {
       res.send(result)
     })
 
+    // jwt
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1h'
+      })
+      res.send({token})
+    })
+
+
+
     // users
     app.post('/users', async ( req, res ) => {
       const userData = req.body;
@@ -127,6 +143,7 @@ async function run() {
       res.send(result)
     })
 
+  
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
